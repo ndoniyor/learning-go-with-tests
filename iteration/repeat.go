705e307f02f7d1569af2921b0fa8f513cce27bc3
ndoniyor <ndoniyor123@gmail.com>
2024-08-10 15:07:06 -0400

    add package and test for iterating

---
+++ iteration/repeat.go
@@ -0,0 +1,15 @@
+package iteration
+
+func Repeat(letter string) string{
+    var repeated string 
+    for i := 0; i < 5; i++ {
+        repeated += letter
+    }
+    return repeated
+}
+
+
+func main(){
+    Repeat("a")
+}
+
